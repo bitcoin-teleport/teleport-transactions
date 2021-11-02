351018a35cf601d78e19e0857b0c8cd7ecabc6c4
mojoX911 <mojojojo48@protonmail.com>
2021-11-02 19:13:32 +0530

    update offer book sync logic
    Offer book sync internal logic is modified to use byte encoded messages
    instead of json encoding.

--- src/offerbook_sync.rs
+++ src/offerbook_sync.rs
@@ -1,9 +1,10 @@
 use tokio::io::BufReader;
 use tokio::net::TcpStream;
-use tokio::prelude::*;
 use tokio::sync::mpsc;
 
 use crate::messages::{GiveOffer, MakerToTakerMessage, Offer, TakerHello, TakerToMakerMessage};
+
+use crate::taker_protocol::{read_message, send_message};
 
 #[derive(Debug, Clone)]
 pub struct OfferAddress {
@@ -18,14 +19,6 @@ const MAKER_HOSTS: [&str; 4] = [
     "localhost:36102",
 ];
 
-fn parse_message(line: &str) -> Option<MakerToTakerMessage> {
-    let message: MakerToTakerMessage = match serde_json::from_str(line) {
-        Ok(r) => r,
-        Err(_e) => return None,
-    };
-    Some(message)
-}
-
 async fn download_maker_offer(host: &str) -> Option<OfferAddress> {
     //TODO add timeouts to deal with indefinite hangs
     let mut socket = match TcpStream::connect(host).await {
@@ -39,53 +32,35 @@ async fn download_maker_offer(host: &str) -> Option<OfferAddress> {
     let (socket_reader, mut socket_writer) = socket.split();
     let mut socket_reader = BufReader::new(socket_reader);
 
-    let mut message_packet = serde_json::to_vec(&TakerToMakerMessage::TakerHello(TakerHello {
-        protocol_version_min: 0,
+    let hello = TakerToMakerMessage::TakerHello(TakerHello {
         protocol_version_max: 0,
-    }))
-    .unwrap();
-    message_packet.push(b'\n');
-    message_packet
-        .append(&mut serde_json::to_vec(&TakerToMakerMessage::GiveOffer(GiveOffer)).unwrap());
-    message_packet.push(b'\n');
-    //TODO error handling here
-    socket_writer.write_all(&message_packet).await.unwrap();
+        protocol_version_min: 0,
+    });
 
-    let mut line1 = String::new();
-    match socket_reader.read_line(&mut line1).await {
-        Ok(0) | Err(_) => {
-            log::trace!(target: "offer_book", "failed to read line");
+    if let Err(_) = send_message(&mut socket_writer, hello).await {
+        log::trace!("Can't send Hello");
+    } else {
+        let give_offer = TakerToMakerMessage::GiveOffer(GiveOffer);
+        if let Err(_) = send_message(&mut socket_writer, give_offer).await {
+            log::trace!("Can't send offer request");
             return None;
         }
-        Ok(_n) => (),
-    };
-    let _makerhello = if let MakerToTakerMessage::MakerHello(m) = parse_message(&line1)? {
-        m
-    } else {
-        log::trace!(target: "offer_book", "wrong protocol message");
-        return None;
-    };
-    log::trace!(target: "offer_book", "maker hello = {:?}", _makerhello);
+    }
 
-    let mut line2 = String::new();
-    match socket_reader.read_line(&mut line2).await {
-        Ok(0) | Err(_) => {
-            log::trace!(target: "oofer_book", "failed to read line2");
+    if let Ok(MakerToTakerMessage::MakerHello(_)) = read_message(&mut socket_reader).await {
+        if let Ok(MakerToTakerMessage::Offer(offer)) = read_message(&mut socket_reader).await {
+            return Some(OfferAddress {
+                offer,
+                address: String::from(host),
+            });
+        } else {
+            log::trace!("Didn't recieve Offer");
             return None;
         }
-        Ok(_n) => (),
-    };
-    let offer = if let MakerToTakerMessage::Offer(o) = parse_message(&line2)? {
-        o
     } else {
-        log::trace!(target: "offer_book", "wrong protocol message2");
+        log::trace!("Didn't recieve Hello");
         return None;
-    };
-
-    Some(OfferAddress {
-        offer,
-        address: String::from(host),
-    })
+    }
 }
 
 pub async fn sync_offerbook() -> Vec<OfferAddress> {
