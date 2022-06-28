04609af85275baeb31e3cbc05ec76a88df932240
chris-belcher <chris-belcher@users.noreply.github.com>
2022-06-28 18:11:42 +0100

    Fix bug in generate-wallet
    If you run "generate-wallet" but the Bitcoin Core wallet "teleport" doesnt
    exist then it will create the file "wallet.teleport"
    
    then when you fix it by creating a Bitcoin Core wallet called "teleport", and
    run generate-wallet again it will complain that "wallet.teleport" already exists

--- src/lib.rs
+++ src/lib.rs
@@ -111,8 +111,6 @@ pub fn generate_wallet(wallet_file_name: &PathBuf) -> std::io::Result<()> {
     let mnemonic =
         mnemonic::Mnemonic::new_random(bitcoin_wallet::account::MasterKeyEntropy::Sufficient)
             .unwrap();
-    Wallet::save_new_wallet_file(&wallet_file_name, mnemonic.to_string(), extension.clone())
-        .unwrap();
 
     let w = match Wallet::load_wallet_from_file(
         &wallet_file_name,
@@ -133,6 +131,9 @@ pub fn generate_wallet(wallet_file_name: &PathBuf) -> std::io::Result<()> {
         &Vec::<_>::new(),
     )
     .unwrap();
+
+    Wallet::save_new_wallet_file(&wallet_file_name, mnemonic.to_string(), extension.clone())
+        .unwrap();
     println!("Write down this seed phrase =\n{}", mnemonic.to_string());
     if !extension.trim().is_empty() {
         println!("And this extension =\n\"{}\"", extension);
